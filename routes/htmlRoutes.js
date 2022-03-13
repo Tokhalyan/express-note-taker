913314c00b199524486827c10fe60d2e0d6eb8c8
Tokhalyan <tokhalyan@gmail.com>
2022-03-13 15:05:57 -0700

    created save data functionality

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,18 @@
+// Dependencies
+const path = require('path');
+const router = require('express').Router();
+
+router.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/notes.html'));
+});
+
+router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
+// If no matching route is found directing to home page
+router.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.use(express.static('public'));
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+// Default response for any other request (Not Found)
+app.use((req, res) => {
+    res.status(404).end();
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port http://localhost:${PORT}`);
+});
